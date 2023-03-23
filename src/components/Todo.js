cfafafb1cee3c541586bc6a0f93ccf10368bdca5
Brendan Moor <bmoor9027@students.mc3.edu>
2023-03-23 19:36:19 -0400

    Todo App

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function Todo() {
+    return(
+        <form>
+            <div>
+                <input/>
+                <button>Add</button>
+            </div>
+            <div>
+                
+            </div>
+        </form>
+    )
+}
+export default Todo;
