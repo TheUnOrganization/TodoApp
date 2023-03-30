0b776d73c38e74ffc267f7f167d9d4c41993b210
Brendan Moor <bmoor9027@students.mc3.edu>
2023-03-30 12:22:00 -0400

    Todo App

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,11 +4,14 @@ function Todo() {
     return(
         <form>
             <div>
-                <input/>
+                <input type="text" placeholder='Enter a todo...'/>
                 <button>Add</button>
             </div>
             <div>
-                
+                <li><input type="checkbox"/>dd</li>
+                <li><input type="checkbox"/>Learn React</li>
+                <li><input type="checkbox"/>Learn Redux</li>
+                <li><input type="checkbox"/>Learn GraphQL</li>
             </div>
         </form>
     )
