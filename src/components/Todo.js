663e30ae53d508facfccb40b15a0b8d787cb7d2f
Brendan Moor <bmoor9027@students.mc3.edu>
2023-03-30 12:36:52 -0400

    Todo App

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,43 @@
 import { useState } from 'react';
+import classes from './Todo.module.css';
+
+const INITIAL_PRODUCTS = [
+    { id: 'p1', title: 'dd'},
+    { id: 'p2', title: 'Learn React'},
+    { id: 'p3', title: 'Learn Redux'},
+    { id: 'p4', title: 'Learn GraphQL'},
+
+]
 
 function Todo() {
+
+    const [products, setProducts] = useState(INITIAL_PRODUCTS)
+
+    function addProductHandler() {
+        setProducts((curProducts) => 
+            curProducts.concat({
+                title: 'Another new Todo',
+            })
+        );
+    }
+
     return(
         <form>
             <div>
                 <input type="text" placeholder='Enter a todo...'/>
                 <button>Add</button>
             </div>
-            <div>
-                <li><input type="checkbox"/>dd</li>
-                <li><input type="checkbox"/>Learn React</li>
-                <li><input type="checkbox"/>Learn Redux</li>
-                <li><input type="checkbox"/>Learn GraphQL</li>
+            <div className={classes.searchbar}>
+                <input type="text" placeholder='Filter for a todo...'/>
             </div>
+            <div>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        {product.title}
+                    </li>
+                ))}
+            </div>
+            <button onClick={addProductHandler}>Add Todo</button>
         </form>
     )
 }
